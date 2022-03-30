50a4114ab19f7258f8b96a4a41844aaf46017475
Alta Steward <63741115+amsteward@users.noreply.github.com>
2022-03-30 15:58:52 -0700

    Update convert_ge.py
    Line 203 divide by zero error (+0.00000000001)

--- dicom2nifti/convert_ge.py
+++ dicom2nifti/convert_ge.py
@@ -200,7 +200,7 @@ def _get_grouped_dicoms(dicom_input):
         # if the stack number decreases we moved to the next stack
         if previous_position is not None:
             current_direction = numpy.array(dicom_.ImagePositionPatient) - previous_position
-            current_direction = current_direction / numpy.linalg.norm(current_direction)
+            current_direction = current_direction / (numpy.linalg.norm(current_direction)+ 0.00000000001)
         if current_direction is not None and \
                 previous_direction is not None and \
                 not numpy.allclose(current_direction, previous_direction, rtol=0.05, atol=0.05):
